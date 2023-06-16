9a7ce00b3b8153e29d55d62e445593ec4a01ab7f
nonice <tomilin07etor@gmail.com>
2023-06-16 19:05:43 +0200

    Bot Ukrainian Localization |Select time zone

--- src/controllers/timezone.controller.js
+++ src/controllers/timezone.controller.js
@@ -6,7 +6,9 @@ const timezoneComposer = new Composer();
 timezoneComposer.command('timezone', (ctx) => {
   ctx.session.inputState = INPUT_STATE_TIMEZONE;
 
-  ctx.reply('Input timezone as number x(-11 <= x <= 14)');
+  ctx.reply(
+    'Введіть різницю вашого часового поясу відносно UTC. \n Як вказано на прикладі x(-11 <= x <= 14)'
+  );
 });
 
 module.exports = {
