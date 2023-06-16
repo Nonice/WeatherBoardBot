053d00823efb56b998a9e628aca46e05be55b0e8
nonice <tomilin07etor@gmail.com>
2023-06-16 18:53:18 +0200

    Bot Ukrainian Localization |Set time zone

--- src/services/timezone.service.js
+++ src/services/timezone.service.js
@@ -2,13 +2,13 @@ async function checkedTimezone({ text, session }) {
   const timezone = Number(text);
 
   if (isNaN(timezone) || timezone < -11 || timezone > 14) {
-    return 'Not valid! Try again';
+    return 'Недійсно! Спробуйте ще раз.';
   }
 
   session.timezone = timezone;
   session.inputState = null;
 
-  return `Setted timezone = ${timezone}`;
+  return `Часовий пояс встановлено UTC+${timezone}`;
 }
 
 module.exports = { checkedTimezone };
